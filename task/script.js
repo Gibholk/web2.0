8733f9e8028f8c1bbed2837b7c1457036821ae71
Gibholk <novitnijmatvej@gmail.com>
2024-10-19 18:04:21 +0300

    feat

---
+++ task/script.js
@@ -0,0 +1,49 @@
+// const number = prompt("Введите число");
+// if (number % 2 == 0){
+//     alert("Чётное");
+// }else{
+//     alert("нечётно");
+// }
+
+
+// const firstNumber = promt("Введите первое число");
+// const secondNumber = prompt("Введите второе число");
+// if (firstNumber == secondNumber){
+//     alert("Равны");
+// }else if(firstNumber > secondNumber){
+//     alert("Первое число больше");
+// }else{
+//     alert("Второе чило больше");
+// }
+
+
+// const correctLogin = "admin";
+// const correctPassword = "secret";
+// const login = prompt("Введите логин");
+// const password = prompt("Введите пароль");
+// if(login == correctLogin && password == correctPassword){
+//     alert("Доступ разрещён");
+// }else {
+//     alert("Доступ запрещён");
+// }
+
+
+// const n = prompt("Введите чило");
+// let sum = 0;
+// let i = 1;
+// while (i <=n){
+//     sum += i;
+//     i++;
+// }
+// alert(`Сумма чисел от 1 до ${n} равна ${sum}`);
+
+
+//  for (let i = 1; i < 3; i++){
+//      console.log(`3 x ${i} = ${3 * i}`);
+//  }
+
+
+// const numbers = [];
+// for(let i = 0; i < 3; i++){
+//     const number = prompt("Введите число");
+// }
