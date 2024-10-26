48a5acbe70bbda60936cf6845fdaf88e6055bae0
Gibholk <novitnijmatvej@gmail.com>
2024-10-26 17:56:18 +0300

    feat

---
+++ object/script.js
@@ -0,0 +1,22 @@
+ let user = {}
+     user.name = "Jhon",
+     user.surname = "Smith",
+     user.name = "Pete"
+     delete user.name;
+     
+ function isEmpty(object){
+     for(let key in object ){
+     return true;
+ }
+ return false;
+}
+
+let salaries = {
+    john: 100,
+    Ann: 160,
+    Pete: 130
+};
+for(let key in salaries){
+    num +=salaries[key]
+}
+console.log(num)
