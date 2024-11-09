6a2cf4ed6b0a22f876ba3ad1c04f2305085a53c0
Gibholk <novitnijmatvej@gmail.com>
2024-11-09 17:54:42 +0300

    hello

--- lesson/script.js
+++ lesson/script.js
@@ -22,10 +22,36 @@
 // };
 
 
-function account(){
-    this.balance =0;
+// function account(){
+//     this.balance =0;
+// }
+// account.prototype.deposit = 
+// function(amount){
+//     this.balance += amount;
+// };
+
+
+// const number = [1, 2, 3, 4, 5];
+// const fruits = ["apple", "banana","orange"];
+// const people = [
+//     {name: "John"},
+//     {name:"Alice"},
+//     {name:"Bob"},
+// ];
+// const words = ["hello","world","javascript"];
+// const numbers = Array.of(1, 2, 3, 4, 5);
+// const thirdElement = numbers[2];
+// console.log(thirdElement);
+// const Number = [1, 2, 3, 4,5];
+// const firstElement = numbers[0];
+// console.log(firstElement);
+
+
+function sumArray(numbers){
+    let sum = 0;
+    for(let i = 0; i < numbers.length;
+        i++){
+            sum += numbers[i];
+        }
+        return sum;
 }
-account.prototype.deposit = 
-function(amount){
-    this.balance += amount;
-};
